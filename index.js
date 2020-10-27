ec3665df2a627c8bcd46f5909cd78d1d990f310a
skanagui <s.kanagui@gmail.com>
2020-10-27 11:01:04 -0400

    3/4 delivs

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
-
-
 /***** Deliverable 2 *****/
+
+const header = document.querySelector('h1').style.color = 'red'
+//console.log("Here's your header:", header)
+
+
+
+
+
+
+
 
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+const list = document.createElement('ul')
+const div = document.querySelector('.player-container')
+
+div.append(list)
+
+
+
+function renderPlayer(playerObj){
+
+    const player = document.createElement("li")
+    player.className = "playerObj"
+
+    player.innerHTML = `
+    <div class="player" data-number= "${playerObj.number}">
+        <h3>
+            ${playerObj.name} (<em>${playerObj.nickname}</em>)
+        </h3>
+        <img src="${playerObj.photo}" alt="${playerObj.name}">
+    </div>
+
+    `
+
+ //console.log("PLAYERS array looks like this:", PLAYERS)
+
+    list.append(player)
+
+
+
+}
+
+function renderAllPlayers (players) {
+    players.forEach(function(playerObj){
+        renderPlayer(playerObj)
+    })
+}
+
+
+
+renderAllPlayers(PLAYERS);
+
+
+
 
 
 /***** Deliverable 4 *****/
